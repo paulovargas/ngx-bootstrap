91882b888dd3067119a1741f9cd57bd4835b9b45
Paulo Tomé Gomes de Vargas <paulotomegomesdevargas@gmail.com>
2024-04-03 21:07:33 -0300

    teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+
+interface ITab {
+  title: string;
+  content: string;
+  removable?: boolean;
+  disabled?: boolean;
+  active?: boolean;
+  customClass?: string;
+}
 
 @Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, TabsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ngx-bootstrap';
+
+  tabs: ITab[] = [
+    {
+      title: 'Dynamic Title 1',
+      content: 'Dynamic content 1',
+      customClass: 'customClass'
+    },
+    {
+      title: 'Dynamic Title 2',
+      content: 'Dynamic content 2',
+      customClass: 'customClass'
+    }
+  ];
 }
