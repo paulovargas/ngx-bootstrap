550878e07dca57dc6b20ad8258a7af9f42f5e1eb
Paulo Tomé Gomes de Vargas <paulotomegomesdevargas@gmail.com>
2024-04-16 22:10:51 -0300

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
 
 interface ITab {
   title: string;
@@ -16,23 +19,41 @@ interface ITab {
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, TabsModule],
+  imports: [
+    AboutComponent,
+    CommonModule,
+    ContactComponent,
+    HomeComponent,
+    RouterOutlet,
+    RouterLink,
+    RouterModule,
+    TabsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  routerActive: any;
   title = 'ngx-bootstrap';
 
   tabs: ITab[] = [
     {
-      title: 'Dynamic Title 1',
-      content: 'Dynamic content 1',
+      title: 'Home',
+      content: 'Home',
       customClass: 'customClass'
     },
     {
-      title: 'Dynamic Title 2',
-      content: 'Dynamic content 2',
+      title: 'Contact',
+      content: 'Contact',
+      customClass: 'customClass'
+    },
+    {
+      title: 'About',
+      content: 'About',
       customClass: 'customClass'
     }
   ];
+rla1: any;
+rla2: any;
+rla3: any;
 }
